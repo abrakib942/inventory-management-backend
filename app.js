6440bb76b889fcce3ad8104daa85c62c346ef69e
abrakib942 <abrakib942@gmail.com>
2022-09-18 23:21:13 +0600

    first commit

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-
+//middleware
 app.use(express.json());
 app.use(cors());
 
-
+//routes
+const productRouter = require("./routes/product.route");
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
 
+//posting database
+
+app.use("/api/v1/product", productRouter);
 
 module.exports = app;
